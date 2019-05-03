49a530881426d73fb43350c4a36301b9b79dfa95
Subat Hayrat <subat.hayrat@gmail.com>
2019-05-03 19:50:07 -0400

    change Procfile

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-var PORT = 3000 || process.env.PORT
 server.use(middlewares)
 server.use(router)
-server.listen(PORT, () => {
+server.listen(process.env.PORT || 4000, () => {
   console.log('JSON Server is running')
 })

611fa0534f8303f5a909b483159aa3b1e114fa48
Subat Hayrat <subat.hayrat@gmail.com>
2019-05-03 20:19:29 -0400

    port changes

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-// server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const PORT = process.env.PORT || 3000;
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(PORT, () => {
-  console.log('JSON Server is running')
-})
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
